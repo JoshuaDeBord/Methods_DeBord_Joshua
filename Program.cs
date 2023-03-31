5867d6b4a6e921ca88877196f85d6cc4278c78d6
JoshuaDeBord <114021926+JoshuaDeBord@users.noreply.github.com>
2023-03-31 14:33:08 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Methods_DeBord_Joshua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a whole number.");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another whole number.");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine(Multyply(6, 8));
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            WelcomeUser(name);
+
+            Goodbye();
+        }
+
+        static int Multyply(int nun1, int num2)
+        {
+            return nun1 * num2;
+        }
+
+
+        static void WelcomeUser(string username)
+        {
+            Console.WriteLine($"Hello, {username}. I am proud that I have met you.");
+
+        }
+
+        static void Goodbye()
+        {
+            Console.WriteLine("have a fantastic day!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Methods_DeBord_Joshua
+ dafjkhwfbvefgvboif vwuvvbi fwjvjxhvjnf gbjfyvgd ioewsi dh0ifch2u4 gf9e4gtvuo2 4yv9sdgusid fjkc iHJ ksfjkashdgk fd jfioah flwh kgbhfgvhrw gjwrihogsv jhfeo oi hffdjwkef iwfbi hoirg br guigrugriuriuvugruuhguhrg uwui orghugf oguv iojre iug hgr igji ghk gjkwy voiqedhgfexr j kdavsdfh kwhdsfucwdmr htvu3enm fcgvj yndi gfuHB JDulek hdfjyDcfsukdfghwjecflgvrkeugnb kerhcfjr gkvfec h
